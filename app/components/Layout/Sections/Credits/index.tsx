23aee0ef29e9bc686db0c014c792f609bedd533d
Matheus <matheus.alves.front.m2@gmail.com>
2024-01-24 22:01:57 -0300

    fix letter

--- app/components/Layout/Sections/Credits/index.tsx
+++ app/components/Layout/Sections/Credits/index.tsx
@@ -4,7 +4,7 @@ import styles from './credits.module.scss'
 export const CreditsSection = forwardRef<HTMLDivElement>(function CreditsSection(props, ref) {
   return (
     <footer className={styles.Footer}>
-      <h3 ref={ref}>Contribuitors:</h3>
+      <h3 ref={ref}>Contributors:</h3>
       <p>Created and Developed by: <a href="https://www.linkedin.com/in/matheus-alves-pereira/">Matheus Alves</a></p>
       <p>Ui/Ux Envolved: <a href="https://www.linkedin.com/in/lucaslopesrp/">Lucas Lopes</a></p>
       <p>3D Model Envolved: <a href="https://www.instagram.com/rhopsis/">Lucas Alves</a></p>
