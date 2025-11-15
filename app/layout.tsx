2b341bea295ef7382c69646917b23fe849ff3398
matheus <matheus.alves.front.m2@gmail.com>
2025-11-15 00:16:32 -0300

    TUTORIAL

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,24 @@ export const metadata: Metadata = {
   title: 'Matheus Alves - Full-Stack',
   description: 'Welcome to my PWA website! Here you can find my socials and check about my skills.',
   manifest: "/manifest.json",
+  openGraph: {
+    title: 'Matheus Alves - Full-Stack',
+    description: 'Welcome to my PWA website! Here you can find my socials and check about my skills.',
+    images: [
+      {
+        url: '/assets/foto-perfil.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Matheus Alves profile picture'
+      }
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Matheus Alves - Full-Stack',
+    description: 'Welcome to my PWA website! Here you can find my socials and check about my skills.',
+    images: ['/assets/foto-perfil.jpg']
+  },
   icons: {
     icon: '/icons/nextjs-neon.png',
     shortcut: '/icons/nextjs-neon.png',
